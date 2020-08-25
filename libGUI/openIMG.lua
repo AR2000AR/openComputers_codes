877c5e8206b06d90bc330f54f927ae8db89c4f61
AR2000AR <AR2000AR@users.noreply.github.com>
2020-08-26 01:04:16 +0200

    update to use the new version of the lib

--- libGUI/openIMG.lua
+++ libGUI/openIMG.lua
@@ -10,7 +10,7 @@ if(fs.exists(args[1]) and not fs.isDirectory(args[1])) then
   local bk = gpu.getBackground()
   gpu.setBackground(tonumber(args[2]or bk,16))
   term.clear()
-  libgui.drawImg(libgui.openPAM(args[1]),1,1)
+  libgui.widget.Image(1,1,args[1]):draw()
   os.sleep(1)
   require("event").pull("key_down")
   gpu.setBackground(bk)
