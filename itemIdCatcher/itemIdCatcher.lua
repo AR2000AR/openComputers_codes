d4bff1caf037d0509375cbedd3682c96ac95d3d9
AR2000AR <AR2000AR@users.noreply.github.com>
2020-08-25 02:53:06 +0200

    fix default file path

--- itemIdCatcher/itemIdCatcher.lua
+++ itemIdCatcher/itemIdCatcher.lua
@@ -5,7 +5,7 @@ local fs = require("filesystem")
 
 local args,opts = shell.parse(...)
 
-local fileName = args[1] or "home/id.txt"
+local fileName = args[1] or "/home/id.txt"
 local openMode = "a"
 if(not fs.exists(fileName)) then openMode = "w" end
 local file = io.open(fileName,openMode)
