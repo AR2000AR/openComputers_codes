3c62ea3573975667c1659821bd5c24c35a71bb8c
AR2000AR <AR2000AR@users.noreply.github.com>
2020-08-25 02:37:25 +0200

    itemIdCatcher

---
+++ itemIdCatcher/openIMG.lua
@@ -0,0 +1,20 @@
+local shell = require("shell")
+local term = require("term")
+local fs = require("filesystem")
+local libgui = require("libgui")
+local gpu = require("component").gpu
+
+local args,opts = shell.parse(...)
+
+if(fs.exists(args[1]) and not fs.isDirectory(args[1])) then
+  local bk = gpu.getBackground()
+  gpu.setBackground(tonumber(args[2]or bk,16))
+  term.clear()
+  libgui.drawImg(libgui.openPAM(args[1]),1,1)
+  os.sleep(1)
+  require("event").pull("key_down")
+  gpu.setBackground(bk)
+  term.clear()
+else
+  print("Not a file")
+end
