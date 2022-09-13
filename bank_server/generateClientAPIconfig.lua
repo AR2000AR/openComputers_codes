2549435cca9d095884af045d3ec3dec7602c3c42
AR2000AR <AR2000AR@users.noreply.github.com>
2022-09-13 19:58:09 +0200

    refactoring

--- bank_server/generateClientAPIconfig.lua
+++ bank_server/generateClientAPIconfig.lua
@@ -6,45 +6,44 @@ local event = require("event")
 local term = require("term")
 local serialization = require("serialization")
 
-local config = {secret="",timeout=5,bank_addr=""}
+local config = {secret = "", timeout = 5, bank_addr = ""}
 
-local args,opts = shell.parse(...)
-if(#args ~= 1 or opts["h"])then
+local args, opts = shell.parse(...)
+if (#args ~= 1 or opts["h"]) then
   print("generateClientSecret clientName")
   os.exit(1)
 end
 
 --get the local (server) address
 local localModemAdd = component.modem.address
-print("Server address : "..localModemAdd)
+print("Server address : " .. localModemAdd)
 
 --get the future client address
 local clientModemAdd = localModemAdd
-while(clientModemAdd == localModemAdd) do
+while (clientModemAdd == localModemAdd) do
   print("Replace the network card with the client one")
-  event.pull(10,"component_available")
+  event.pull(10, "component_available")
   clientModemAdd = component.modem.address
-  if(clientModemAdd == localModemAdd)then
+  if (clientModemAdd == localModemAdd) then
     print("Error : same network card")
     term.write("Cancel [y/N]")
     local userInput = term.read()
-    if(userInput == "y\n" or userInput == "Y\n")then
+    if (userInput == "y\n" or userInput == "Y\n") then
       os.exit(1)
     end
   end
 end
-print("Client address : "..clientModemAdd)
+print("Client address : " .. clientModemAdd)
 
-shell.execute("generateClientSecret "..clientModemAdd.." "..args[1]..".secret")
-local secretFile = io.open(args[1]..".secret","r")
+shell.execute("generateClientSecret " .. clientModemAdd .. " " .. args[1] .. ".secret")
+local secretFile = io.open(args[1] .. ".secret", "r")
+assert(secretFile, string.format("Could not open file : %s.secret", args[1]))
 local secretString = secretFile:read("*a")
 secretFile:close()
 
 config.bank_addr = localModemAdd
 config.secret = secretString
 
-print(config)
-
-local apiConfigFile = io.open(args[1]..".conf","w")
+local apiConfigFile = io.open(args[1] .. ".conf", "w")
+assert(apiConfigFile, string.format("Could not open file : %s.conf", args[1]))
 apiConfigFile:write(serialization.serialize(config)):close()
-print(done)
--- bank_server/generateClientSecret.lua
+++ bank_server/generateClientSecret.lua
@@ -1,12 +1,12 @@
 local data = require("component").data
 local io = require("io")
 local shell = require("shell")
-local fs=require("filesystem")
+local fs = require("filesystem")
 local serialization = require("serialization")
 
 local CONF_DIR = "/etc/bank/server/"
 local CONF_FILE_NAME = "conf.cfg"
-local keyFile = CONF_DIR.."key" --default value
+local keyFile = CONF_DIR .. "key" --default value
 
 local function help()
   print("generateClientSecret uuid [file]")
@@ -15,26 +15,29 @@ end
 local function getKey(public)
   local ext = ".priv"
   local type = "ec-private"
-  if(public) then
-    ext=".pub"
+  if (public) then
+    ext = ".pub"
     type = "ec-public"
   end
-  local file = io.open(keyFile..ext,"r")
-  local key = data.deserializeKey(data.decode64(file:read("*a")),type)
+  local file = io.open(keyFile .. ext, "r")
+  assert(file, string.format("Could not open file : %s", keyFile .. ext))
+  local key = data.deserializeKey(data.decode64(file:read("*a")), type)
   file:close()
   return key
 end
 
-local args,opts = shell.parse(...)
-if(fs.exists(CONF_DIR..CONF_FILE_NAME))then --read the config file
-  local file=io.open(CONF_DIR..CONF_FILE_NAME,"r")
+local args, opts = shell.parse(...)
+if (fs.exists(CONF_DIR .. CONF_FILE_NAME)) then --read the config file
+  local file = io.open(CONF_DIR .. CONF_FILE_NAME, "r")
+  assert(file, string.format("Could not open file : %s", CONF_DIR .. CONF_FILE_NAME))
   local confTable = serialization.unserialize(file:read("*a"))
   file:close()
-  if(confTable.keyFile)then keyFile = confTable.keyFile end
+  if (confTable.keyFile) then keyFile = confTable.keyFile end
 end
-if(args[1]) then
-  local secret = data.ecdsa(args[1],getKey(false))
-  local secretFile = io.open(args[2] or "/home/secret","w")
+if (args[1]) then
+  local secret = data.ecdsa(args[1], getKey(false))
+  local secretFile = io.open(args[2] or "/home/secret", "w")
+  assert(secretFile, string.format("Could not open file : %s", args[2] or "/home/secret"))
   secretFile:write(data.encode64(secret))
   secretFile:close()
 else
