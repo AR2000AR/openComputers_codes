a10a1570cb789dac23210bc21e2bda1465318d30
AR2000AR <AR2000AR@users.noreply.github.com>
2022-09-17 17:13:57 +0200

    [libgui] removved old comment

--- libGUI/libGUI.lua
+++ libGUI/libGUI.lua
@@ -4,13 +4,9 @@ local uuid = require("uuid")
 local class = require("libClass")
 local fs = require("filesystem")
 
-local libGUI = {widget={}}
---libGUI.widget.Widget = require("libGUI/widget/Widget")
---libGUI.widget.Rectangle = require("libGUI/widget/Rectangle")
---libGUI.widget.Image = require("libGUI/widget/Image")
---libGUI.widget.Text = require("libGUI/widget/Text")
+local libGUI = {widget = {}}
 for widgetFileName in fs.list("/usr/lib/libGUI/widget") do
-  libGUI.widget[widgetFileName:sub(1,#widgetFileName-4)] = require("libGUI/widget/"..widgetFileName:sub(1,#widgetFileName-4))
+  libGUI.widget[widgetFileName:sub(1, #widgetFileName - 4)] = require("libGUI/widget/" .. widgetFileName:sub(1, #widgetFileName - 4))
 end
 libGUI.Screen = require("libGUI/Screen")
 libGUI.Image = require("libGUI/ImageFile")
