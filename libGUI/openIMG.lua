58fd1ac55ffc03a001b17554dadc5dd0a4ef38d9
AR2000AR <AR2000AR@users.noreply.github.com>
2023-01-16 00:59:43 +0100

    quality of life

--- libGUI/openIMG.lua
+++ libGUI/openIMG.lua
@@ -4,19 +4,27 @@ local fs = require("filesystem")
 local libgui = require("libGUI")
 local gpu = require("component").gpu
 
+local oldX, oldY = gpu.getResolution()
+
 local args, opts = shell.parse(...)
+args[1] = shell.resolve(args[1])
 
 if (fs.exists(args[1]) and not fs.isDirectory(args[1])) then
   local bk = gpu.getBackground()
-  local drawMethod = false
+  local drawMethod = true
   if (args[3] ~= nil and args[3]:lower() == "true") then drawMethod = true end
   gpu.setBackground(tonumber(args[2] or "") or bk)
   term.clear()
-  libgui.widget.Image(1, 1, args[1], drawMethod):draw()
+  local screen = libgui.Screen()
+  local img = libgui.widget.Image(1, 1, args[1], drawMethod)
+  gpu.setResolution(img:getWidth(), img:getHeight())
+  screen:addChild(img)
+  screen:draw()
   ---@diagnostic disable-next-line: undefined-field
   os.sleep(1)
   require("event").pull("key_down")
   gpu.setBackground(bk)
+  gpu.setResolution(oldX, oldY)
   term.clear()
 else
   print("Not a file")
