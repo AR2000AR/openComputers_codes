3cfc48a7ad9be8420848a8acdfa173106339e3d2
AR2000AR <AR2000AR@users.noreply.github.com>
2023-01-16 18:38:47 +0100

    [bank_server] qol

--- bank_server/generateClientAPIconfig.lua
+++ bank_server/generateClientAPIconfig.lua
@@ -5,12 +5,14 @@ local component = require("component")
 local event = require("event")
 local term = require("term")
 local serialization = require("serialization")
+local fs = require("filesystem")
 
 local config = {secret = "", timeout = 5, bank_addr = ""}
 
 local args, opts = shell.parse(...)
 if (#args ~= 1 or opts["h"]) then
-  print("generateClientSecret clientName")
+  print("generateClientSecret [-n] clientName")
+  print("\t -n : do not generate secret")
   os.exit(1)
 end
 
@@ -18,31 +20,38 @@ end
 local localModemAdd = component.modem.address
 print("Server address : " .. localModemAdd)
 
---get the future client address
-local clientModemAdd = localModemAdd
-while (clientModemAdd == localModemAdd) do
-  print("Replace the network card with the client one")
-  event.pull(10, "component_available")
-  clientModemAdd = component.modem.address
-  if (clientModemAdd == localModemAdd) then
-    print("Error : same network card")
-    term.write("Cancel [y/N]")
-    local userInput = term.read()
-    if (userInput == "y\n" or userInput == "Y\n") then
-      os.exit(1)
+if (not opts["n"]) then
+  --get the future client address
+  local clientModemAdd = localModemAdd
+  while (clientModemAdd == localModemAdd) do
+    print("Replace the network card with the client one")
+    event.pull(10, "component_available")
+    clientModemAdd = component.modem.address
+    if (clientModemAdd == localModemAdd) then
+      print("Error : same network card")
+      term.write("Cancel [y/N]")
+      local userInput = term.read()
+      if (userInput == "y\n" or userInput == "Y\n") then
+        os.exit(1)
+      end
     end
   end
-end
-print("Client address : " .. clientModemAdd)
+  print("Client address : " .. clientModemAdd)
 
-shell.execute("generateClientSecret " .. clientModemAdd .. " " .. args[1] .. ".secret")
-local secretFile = io.open(args[1] .. ".secret", "r")
-assert(secretFile, string.format("Could not open file : %s.secret", args[1]))
-local secretString = secretFile:read("*a")
-secretFile:close()
+  shell.execute("generateClientSecret " .. clientModemAdd .. " " .. args[1] .. ".secret")
+  local secretFile = io.open(args[1] .. ".secret", "r")
+  assert(secretFile, string.format("Could not open file : %s.secret", args[1]))
+  local secretString = secretFile:read("*a")
+  secretFile:close()
+
+  local path = args[1] .. ".secret"
+  if (not fs.exists(path)) then path = shell.resolve(path) end
+  fs.remove(path)
+
+  config.secret = secretString
+end
 
 config.bank_addr = localModemAdd
-config.secret = secretString
 
 local apiConfigFile = io.open(args[1] .. ".conf", "w")
 assert(apiConfigFile, string.format("Could not open file : %s.conf", args[1]))
