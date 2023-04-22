d18f2722adc2e8da991aa1b76d4ab9b4c99f363b
AR2000AR <AR2000AR@users.noreply.github.com>
2023-04-22 18:39:27 +0200

    [libClass2] yes, I made 2 of them. But this one is better
    Inheritance, polymorphism and instanceof

---
+++ libClass2/lib/libClass2.lua
@@ -0,0 +1,47 @@
+---@class Object
+---@field parent Object the parent class
+local Object = {}
+
+function Object:new()
+    return setmetatable({}, {__index = self})
+end
+
+---Test if the object is a instance of a class
+---@param C Object
+---@return boolean
+function Object:instanceOf(C)
+    if (not getmetatable(C).isClass) then error("Argument #1 is not a class or object", 2) end
+    if (getmetatable(self).__index == C) then
+        return true
+    else
+        return getmetatable(self).__index ~= Object and getmetatable(self).__index:instanceOf(C)
+    end
+end
+
+setmetatable(Object, {
+    isClass = true,
+    __call = function(self, ...)
+        return self:new(...)
+    end,
+}
+)
+--=============================================================================
+
+---Create a new class
+---@generic C:Object
+---@param parent? C Class inherited from
+---@return unknown Really is a new class type.
+local function class(parent)
+    parent = parent or Object
+
+    local mt = {
+        isClass = true,
+        __call = function(self, ...)
+            return self:new(...)
+        end,
+        __index = parent,
+    }
+    return setmetatable({parent = parent}, mt)
+end
+
+return class
