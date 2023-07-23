954fc34e271946b6f104f683a9897cd56f901302
Renno231 <75190549+Renno231@users.noreply.github.com>
2023-07-22 19:39:42 -0500

    turned toggle switch into widget and got rid of child rectangle
    AR2000, I know you won't like how I have handled the switch ;)

--- yawl/lib/yawl/widget/ToggleSwitch.lua
+++ yawl/lib/yawl/widget/ToggleSwitch.lua
@@ -1,14 +1,15 @@
 local gpu = require("component").gpu
 local class = require("libClass2")
+local Widget = require("yawl.widget.Widget")
 local Frame = require("yawl.widget.Frame")
-local Text = require("yawl.widget.Text")
+--local Text = require("yawl.widget.Text")
 local Rectangle = require("yawl.widget.Rectangle")
 
 ---@class ToggleSwitch:Widget
 ---@field private _size Size
 ---@operator call:ToggleSwitch
 ---@overload fun(parent:Frame,x:number,y:number,width:number,height:number,backgroundColor:number)
-local ToggleSwitch = class(Frame)
+local ToggleSwitch = class(Widget)
 
 ---Create a new ToggleSwitch
 ---@param parent Frame
@@ -29,15 +30,16 @@ function ToggleSwitch:new(parent, x, y, width, height, backgroundColor, foregrou
     local o = self.parent(parent, x, y)
     setmetatable(o, {__index = self})
     o._value = false
-    o._visual = Rectangle(o, 1, 1, math.min(width, height), height, foregroundColor or 0)
+    o._slider = {x = 0, width = height, height = height, backgroundColor = foregroundColor}
     o:speed(2)
+    o.backgroundColor = Frame.backgroundColor
     ---@cast o ToggleSwitch
     o:size(math.max(2, width), height)
     o:backgroundColor(backgroundColor or 0xffffff) --testing defaults
     return o
 end
 
-function ToggleSwitch:set(value)
+function ToggleSwitch:value(value)
     checkArg(1, value, 'boolean', 'nil')
     local oldValue = self._value
     if (value~=nil) then self._value = value end
@@ -47,28 +49,55 @@ end
 function ToggleSwitch:speed(newspeed)
     checkArg(1, newspeed, 'number', 'nil')
     local oldValue = self._speed
-    if (newspeed) then self._speed = math.max(newspeed, 1) / 10 end
+    if (newspeed) then self._speed = math.max(newspeed, 1) end
     return oldValue
 end
 
 function ToggleSwitch:size(width, height)
-    self._visual:size(height, height)
+    self._slider.width, self._slider.height = height, height
+    return self.parent.size(self, width, height)
+end
+
+function ToggleSwitch:switchSize(width, height) --fix later
+    self._slider.width, self._slider.height = height, height
     return self.parent.size(self, width, height)
 end
 
 function ToggleSwitch:toggle()
-    return self:set(not self._value)
+    return self:value(not self:value())
+end
+
+function ToggleSwitch:activeBackgroundColor(value)
+    checkArg(1, value, 'number', 'nil')
+    local oldValue = self._activeBackgroundColor
+    if (value ~= nil) then self._activeBackgroundColor = value end
+    return oldValue
 end
 
 function ToggleSwitch:draw()
     if (not self:visible()) then return end
-    local visual = self._visual
-    local x, step = visual:x(), (self._value and 1 or -1) * math.max(1, self._size.width * self._speed)
-    local boundary = math.max(math.min(self._size.width-visual:width()+1, x+step), 1)
-    if boundary~=x then --
-        visual:position(boundary, 1)
+    --self. = {x = 0, widthheight}
+    local slider = self._slider
+    local width, height = self:width(), self:height()
+    local x, y = self:absX(), self:absY()
+    local sliderX, step = slider.x, (self:value() and 1 or -1) * math.max(1, width * self:speed() / 10)
+    local boundary = math.max(math.min(width-slider.width, sliderX+step), 0)
+    local oldBG = gpu.getBackground()
+    gpu.setBackground(self:backgroundColor())
+    gpu.fill(x, y, width, height, " ")
+    gpu.setBackground(slider.backgroundColor)
+    gpu.fill(x + boundary , y, slider.width, slider.height, " ")
+    if boundary ~= sliderX then 
+        slider.x=boundary
     end
-    self.parent.draw(self)
+    local activeBG = self:activeBackgroundColor()
+    if activeBG and boundary-1 > 0 then
+        gpu.setBackground(activeBG)
+        gpu.fill(x,y, boundary, self:height(), " ")
+    end
+    --if boundary ~= sliderX then --
+    --end
+    gpu.setBackground(oldBG)
 end
 
 return ToggleSwitch
