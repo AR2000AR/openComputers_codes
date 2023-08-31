bf7f7580b1ac21e4589e6d51daca13b58122fa13
AR2000AR <AR2000AR@users.noreply.github.com>
2023-08-31 17:22:16 +0200

    [osinetwork] moved tcp() declaration

--- network/lib/socket/init.lua
+++ network/lib/socket/init.lua
@@ -1,6 +1,12 @@
+local TCPSocket = require("socket.tcp")
+
 return {
     udp = require("socket.udp"),
-    tcp = require("socket.tcp"),
+    ---Creates and returns an TCP master object. A master object can be transformed into a server object with the method listen (after a call to bind) or into a client object with the method connect. The only other method supported by a master object is the close method.
+    ---
+    ---In case of success, a new master object is returned. In case of error, nil is returned, followed by an error message.
+    ---@return TCPSocket
+    tcp = function() return TCPSocket() end,
     ---@type socketdns
     dns = select(2, pcall(require, "socket.dns"))
 }
