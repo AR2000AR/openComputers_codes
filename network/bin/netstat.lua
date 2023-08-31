237a7a321d52300bb011537b1cb5b0856ba9f088
AR2000AR <AR2000AR@users.noreply.github.com>
2023-08-31 17:21:18 +0200

    [osinetwork] replace 0 with *

--- network/bin/netstat.lua
+++ network/bin/netstat.lua
@@ -4,8 +4,8 @@ local ipv4Address = require("network.ipv4").address
 --=============================================================================
 
 for _, info in pairs(network.udp:getInterface():getOpenPorts()) do
-    print(string.format("UDP\t%s:%d\t%s:%d", ipv4Address.tostring(info.loc.address), info.loc.port, ipv4Address.tostring(info.rem.address), info.rem.port))
+    print(string.format("UDP\t%s:%s\t%s:%s", ipv4Address.tostring(info.loc.address):gsub("0.0.0.0", "*"), info.loc.port ~= 0 and info.loc.port or "*", ipv4Address.tostring(info.rem.address):gsub("0.0.0.0", "*"), info.rem.port ~= 0 and info.rem.port or "*"))
 end
 for _, info in pairs(network.tcp:getInterface():getOpenPorts()) do
-    print(string.format("TCP\t%s:%d\t%s:%d\t%s", ipv4Address.tostring(info.loc.address), info.loc.port, ipv4Address.tostring(info.rem.address), info.rem.port, info.state))
+    print(string.format("TCP\t%s:%s\t%s:%s\t%s", ipv4Address.tostring(info.loc.address):gsub("0.0.0.0", "*"), info.loc.port ~= 0 and info.loc.port or "*", ipv4Address.tostring(info.rem.address):gsub("0.0.0.0", "*"), info.rem.port ~= 0 and info.rem.port or "*", info.state))
 end
